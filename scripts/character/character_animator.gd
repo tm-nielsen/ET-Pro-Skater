5db586c4177425623408b9016ce40960a76de0cd
Taylor Nielsen <littleb88888888@gmail.com>
2023-12-25 13:35:04 -0700

    Improved character animator
    - refactored / improved code organization
    - added tween to turns and tilts
    - organized exports

--- scripts/character/character_animator.gd
+++ scripts/character/character_animator.gd
@@ -5,31 +5,59 @@ extends Node3D
 @export var physics_body: CharacterBody3D = get_parent()
 
 @export_group("Parameters")
+@export_subgroup("crouch", "crouch")
 @export var crouch_scale := Vector3(1.2, 0.4, 1.6)
-@export var crouch_duration := 0.15
+@export var crouch_tween_duration := 0.15
+
+@export_subgroup("jump", "jump")
 @export var jump_scale := Vector3(0.6, 1.8, 0.4)
-@export var jump_duration := 0.05
+@export var jump_tween_duration := 0.05
+
+@export_subgroup("turn", "turn")
 @export var turn_angle := 0.25
-@export var tilt_offset := Vector3(0, 0.4, 0.1)
+@export var turn_tween_duration := 0.3
+
+@export_subgroup("tilt", "tilt")
+@export var tilt_offset := Vector2(0.1, 0.4)
 @export var tilt_angle := 0.6
+@export var tilt_tween_duration := 0.2
 
 
 func _physics_process(_delta):
-    var horizontal_input_axis = Input.get_axis("left", "right")
+    update_axis("left", "right", start_turn_tween)
+    update_axis("up", "down", start_tilt_tween)
+    process_crouch_tweens()
+
+
+func update_axis(action_a_name: StringName, action_b_name: StringName, update_method: Callable):
+    var a_toggled = Input.is_action_just_pressed(action_a_name) || Input.is_action_just_released(action_a_name)
+    var b_toggled = Input.is_action_just_pressed(action_b_name) || Input.is_action_just_released(action_b_name)
+    if a_toggled || b_toggled:
+        update_method.call()
+
+
+func start_turn_tween():
+    var turn_direction = Input.get_axis("right", "left")
     if !physics_body.is_on_floor():
-        horizontal_input_axis = 0
-    rotation.z = -horizontal_input_axis * turn_angle
+        turn_direction = 0
+    tween_property("rotation:z", turn_direction * turn_angle, turn_tween_duration)
 
-    var vertical_input_axis = Input.get_axis("down", "up")
-    rotation.x = tilt_angle * -vertical_input_axis
-    position.y = tilt_offset.y if (vertical_input_axis != 0) else 0.0
-    position.z = -vertical_input_axis * tilt_offset.z
 
+func start_tilt_tween():
+    var tilt_direction = Input.get_axis("up", "down")
+    tween_property("rotation:x", tilt_angle * tilt_direction, tilt_tween_duration)
+    var offset_position = position
+    offset_position.y = tilt_offset.y * abs(tilt_direction)
+    offset_position.z = tilt_direction * tilt_offset.x
+    tween_property("position", offset_position, tilt_tween_duration)
+
+
+func process_crouch_tweens():
     if Input.is_action_just_pressed("crouch"):
-        tween_property("scale", crouch_scale, crouch_duration, crouch_bone)
+        tween_property("scale", crouch_scale, crouch_tween_duration, crouch_bone)
     elif Input.is_action_just_released("crouch"):
-        var tween = tween_property("scale", jump_scale, crouch_duration, crouch_bone)
-        tween.tween_property(crouch_bone, "scale", Vector3.ONE, crouch_duration)
+        var tween = tween_property("scale", jump_scale, crouch_tween_duration, crouch_bone)
+        tween.tween_property(crouch_bone, "scale", Vector3.ONE, crouch_tween_duration)
 
 
 func tween_property(property_name: String, final_value, duration: float, target_node = self) -> Tween:
