e9d646a197ec17e6100d14ef2c1371acfd387594
Taylor Nielsen <littleb88888888@gmail.com>
2023-12-27 20:24:10 -0700

    Added sketch of bobblehead script

---
+++ scripts/character/polish/bobblehead.gd
@@ -0,0 +1,43 @@
+class_name BobbleHead
+extends Node3D
+
+@export var neck_node: Node3D
+@export var movement_sensitivity := Vector3(1.0, 0.5, 1.0)
+@export var elasticity := 0.1
+@export_range(0, 1) var damping := 0.9
+
+
+var angular_velocity: Vector3
+var y_scale_velocity: float
+
+var previous_position: Vector3
+var previous_velocity: Vector3
+
+
+func _physics_process(_delta):
+    global_position = neck_node.global_position
+    var velocity = global_position - previous_position
+    rotation += angular_velocity
+    angular_velocity -= rotation * elasticity
+    angular_velocity *= damping
+
+    # position.x = -rotation.z * 0.1
+    # position.z = rotation.x * 0.1
+
+    scale.y += y_scale_velocity
+    y_scale_velocity += (1.0 - scale.y) * elasticity
+    y_scale_velocity *= damping
+
+    var acceleration = velocity - previous_velocity
+
+    var vertical_acceleration = acceleration.dot(neck_node.basis.y)
+    y_scale_velocity += vertical_acceleration * movement_sensitivity.y
+
+    var longitudinal_acceleration = acceleration.dot(neck_node.basis.z)
+    angular_velocity.x += longitudinal_acceleration * movement_sensitivity.z
+
+    var transverse_acceleration = acceleration.dot(neck_node.basis.x)
+    angular_velocity.z += transverse_acceleration * movement_sensitivity.z
+
+    previous_position = global_position
+    previous_velocity = velocity
