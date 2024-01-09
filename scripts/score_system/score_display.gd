0126b8ed6a76df8bbabb3f5c6cdb7f5fa7a3a792
Taylor Nielsen <littleb88888888@gmail.com>
2024-01-08 20:57:51 -0700

    Implemented rudimentary score display

--- scripts/score_system/score_display.gd
+++ scripts/score_system/score_display.gd
@@ -1,6 +1,16 @@
 class_name ScoreDisplay
-extends Node3D
+extends Control
 
+@export_subgroup("references")
+@export var score_label: Label
+@export var multiplier_label: Label
+@export var increment_list_parent: Control
+
+
+func _ready():
+    score_label.text = str(0)
+    multiplier_label.text = "1.0x"
+    _clear_increment_list()
 
 
 func start_held_trick(trick_name: String):
@@ -14,14 +24,24 @@ func update_held_trick_score(score: float):
 
 
 func add_potential_score(trick_name: String, score: int):
-    pass
+    var increment_label = Label.new()
+    increment_label.text = "+%d %s" %[score, trick_name]
+    increment_list_parent.add_child(increment_label)
 
 func bank_score(added_score: int, total_score: int):
-    pass
+    # TODO: clear incrment list after delay
+    # TODO: clearly display score increase
+    score_label.text = str(total_score)
+    _clear_increment_list()
+
+func _clear_increment_list():
+    for child in increment_list_parent.get_children():
+        child.queue_free()
 
 
 func update_multiplier_display(multiplier: float):
-    pass
+    # TODO: add animated size bump
+    multiplier_label.text = "%.1fx" % multiplier
 
 func reset_multiplier_display():
-    pass
+    multiplier_label.text = "1.0x"
