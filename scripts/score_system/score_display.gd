4dc180e1f328631a81702f64deb5c71f4399693c
Taylor Nielsen <littleb88888888@gmail.com>
2024-01-09 14:01:10 -0700

    Added score increase display
    - added score increase label to show when potential score is banked
    - added tween to total score display when it is increased

--- scripts/score_system/score_display.gd
+++ scripts/score_system/score_display.gd
@@ -3,14 +3,24 @@ extends Control
 
 @export_subgroup("references")
 @export var score_label: Label
+@export var score_addition_label: Label
 @export var multiplier_label: Label
 @export var multiplier_lifetime_bar: ProgressBar
 @export var increment_list_parent: Control
+
+@export_subgroup("parameters")
+@export var addition_label_display_duration := 0.8
+@export var score_increase_duration := 0.25
+
+var previous_score: int
+var score_tween: Tween
+var score_addition_tween: Tween
 
 
 func _ready():
     score_label.text = str(0)
     reset_multiplier_display()
+    _hide_score_addition_display()
     _clear_increment_list()
 
 
@@ -31,9 +41,40 @@ func add_potential_score(trick_name: String, score: int):
 
 func bank_score(added_score: int, total_score: int):
     # TODO: clear incrment list after delay
-    # TODO: clearly display score increase
-    score_label.text = str(total_score)
+    _show_score_addition(added_score)
+    update_score_display(total_score)
     _clear_increment_list()
+
+func update_score_display(score: int):
+    if score_tween:
+        score_tween.kill()
+    score_tween = create_tween()
+    score_tween.tween_method(_update_score_display, previous_score, score, score_increase_duration)
+    previous_score = score
+
+func _update_score_display(score: int):
+    score_label.text = _get_seperated_number_string(score)
+
+
+func _show_score_addition(added_score: int):
+    if added_score <= 0:
+        return
+
+    var addition_text = _get_seperated_number_string(added_score)
+    score_addition_label.text = addition_text + " +"
+    score_addition_label.visible = true
+    _hide_score_addition_display_after_delay()
+
+func _hide_score_addition_display_after_delay():
+    if score_addition_tween:
+        score_addition_tween.kill()
+    score_addition_tween = create_tween()
+    score_addition_tween.tween_interval(addition_label_display_duration)
+    score_addition_tween.tween_callback(_hide_score_addition_display)
+
+func _hide_score_addition_display():
+    score_addition_label.visible = false
+
 
 func _clear_increment_list():
     for child in increment_list_parent.get_children():
@@ -56,3 +97,12 @@ func reset_multiplier_display():
 func on_crash():
     reset_multiplier_display()
     _clear_increment_list()
+
+
+func _get_seperated_number_string(number: float):
+    var text = str(int(number))
+    var i = 3
+    while text.length() > i:
+        text = text.insert(text.length() - i, ",")
+        i += 4
+    return text
